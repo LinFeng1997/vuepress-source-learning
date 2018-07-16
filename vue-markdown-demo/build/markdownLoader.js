2edea6b55b7786a501607f4cc4eb523c4a11b353
xuyunfeng <xuyunfeng.fe@bytedance.com>
2018-07-16 14:09:24 +0800

    add markdown-it config

--- vue-markdown-demo/build/markdownLoader.js
+++ vue-markdown-demo/build/markdownLoader.js
@@ -1,11 +1,15 @@
 const markdown = require('markdown-it')
 
 module.exports = function(src) {
-  const html = markdown().render(src)
+  const md = markdown({
+    html: true,
+    typographer: true,
+  })
+  const html = md.render(src)
 
   return (
     `<template>\n` +
-    	`<div class="markdown">${html}</div>\n` +
+    `<div class="markdown">${html}</div>\n` +
     `</template>\n`
   )
 }
